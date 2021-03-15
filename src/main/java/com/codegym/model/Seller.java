a93a662a199cbc7e4f1022c342e0ecd72aef5b0e
Qui Nguyễn Khắc <ngocqui196>
2021-03-15 22:44:33 +0700

    hoan thien ket cau

--- src/main/java/com/codegym/model/Seller.java
+++ src/main/java/com/codegym/model/Seller.java
@@ -1,4 +1,92 @@
 package com.codegym.model;
 
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "seller")
 public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    private String name;
+    private String address;
+    private String avatar;
+    private String phoneNumber;
+    private String userName;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "seller_role",
+            joinColumns = @JoinColumn(name = "seller_id",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_role_id",referencedColumnName = "id")
+    )
+    private List<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
 }
